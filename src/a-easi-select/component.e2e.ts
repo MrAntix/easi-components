10ea97f1364a87a91283b0c9dc66e987e5beec95
Anthony Johnston <aj@antix.co.uk>
2019-06-01 22:38:56 +0100

    test(easi-select): check click works

--- src/a-easi-select/component.e2e.ts
+++ src/a-easi-select/component.e2e.ts
@@ -1,12 +1,31 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage, E2EPage, E2EElement } from '@stencil/core/testing';
 
 describe('a-easi-select', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
+  let page: E2EPage;
+  let element: E2EElement;
+
+  const options = [0, 1, 2];
+
+  beforeEach(async () => {
+    page = await newE2EPage();
 
     await page.setContent('<a-easi-select></a-easi-select>');
-    const element = await page.find('a-easi-select');
+    element = await page.find('a-easi-select');
+    element.setProperty('options', options);
 
+    await page.waitForChanges();
+  });
+
+  it('renders', async () => {
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('select on click', async () => {
+    const option = await page.find('a-easi-select >>> label:nth-child(2)');
+    await option.click();
+
+    const value = await element.getProperty('value');
+
+    expect(value).toBe(options[1]);
   });
 });
